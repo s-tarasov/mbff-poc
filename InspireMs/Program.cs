3846538e91a32be4d1cfeeaa4a553ea03526ce9b
Stanislav Tarasov <mr.netpilgrim@gmail.com>
2023-11-21 23:32:09 +0300

    cleanup InspireMs

--- InspireMs/Program.cs
+++ InspireMs/Program.cs
@@ -7,49 +7,6 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-/*
-
-        new ProductVariant (
-            sku: "t_porsche",
-            name: "Porsche-Diesel Master 319",
-            image: "/static/ProductMs/images/tractor-red.jpg",
-            price: 66m
-        ),
-        new ProductVariant (
-            sku: "t_fendt",
-            name: "Fendt F20 Dieselroß",
-            image: "/static/ProductMs/images/tractor-green.jpg",
-            price: 54m
-        ),
-        new ProductVariant (
-            sku: "t_eicher",
-            name: "Eicher Diesel 215/16",
-            image: "/static/ProductMs/images/tractor-blue.jpg",
-            price: 58m
-        )
-    }),
-     new Product(2, "trailer", new[]
-    {
-        new ProductVariant (
-            sku: "trailer_red",
-            name: "trailer_red",
-            image: "/static/ProductMs/images/trailer-red.jpg",
-            price: 1m
-        ),
-        new ProductVariant (
-            sku: "trailer_green",
-            name: "trailer_green",
-            image: "/static/ProductMs/images/trailer-green.jpg",
-            price: 2m
-        ),
-        new ProductVariant (
-            sku: "trailer_blue",
-            name: "trailer_blue",
-            image: "/static/ProductMs/images/trailer-blue.jpg",
-            price: 3m
-        )
-    })
-*/
 var productToRecomendations = new Dictionary<int, Recomendation[]>
 {
  [1] = new [] { 
